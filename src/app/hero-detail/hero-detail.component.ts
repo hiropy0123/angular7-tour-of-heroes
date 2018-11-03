88b6d87bd5fcb40e5088e2326fb09360687eec17
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-11-03 10:55:09 +0900

    3. Master/Detail Components

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import Hero from '../hero';
+
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.scss']
+})
+export class HeroDetailComponent implements OnInit {
+  // 親コンポーネントからデータの受け取り
+  @Input() hero: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
