636d3130ed99b771801a68fd5233da84b76ec963
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-11-04 19:47:01 +0900

    4. Services | MessageService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,16 +2,20 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  // Inject Service ( "service-in-service" )
+  constructor(private messageService: MessageService) { }
 
   // heroesの取得
   getHeroes(): Observable<Hero[]> {
+    // message
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.scss']
+})
+export class MessagesComponent implements OnInit {
+
+  // テンプレートのコンポーネントにバインドするため'public'でなければならない
+  constructor(public messageService: MessageService) { }
+
+  ngOnInit() {
+  }
+
+}
